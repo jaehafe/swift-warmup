07951ce7bf6ad6d66b2438f329675290054ada45
Jaeha Lee <wogkdkrm112@gmail.com>
2024-08-19 20:47:12 +0900

    feat: 리스트 추가, 삭제

--- WarmUp/WarmUp/02/ListLoop.swift
+++ WarmUp/WarmUp/02/ListLoop.swift
@@ -15,24 +15,61 @@ struct Fruit: Identifiable {
 }
 
 struct ListLoop: View {
-    
-    var favoriteFruits = [
+
+    @State var favoriteFruits = [
         Fruit(name: "Apple", matchFruitName: "banana", price: 1000),
         Fruit(name: "Banana", matchFruitName: "cherry", price: 3000),
         Fruit(name: "cherry", matchFruitName: "double kiwi", price: 4000),
         Fruit(name: "elder berry", matchFruitName: "apple", price: 2000),
     ]
-    
+
+    @State var fruitName: String = ""
+
+    func deleteFruit(at offsets: IndexSet) {
+        favoriteFruits.remove(atOffsets: offsets)
+    }
+
     var body: some View {
         NavigationStack {
-            List(favoriteFruits) { fruit in
+
+            VStack {
+
                 HStack {
-                    Text(fruit.name)
-                    Spacer()
-                    Text("\(fruit.price)원")
+                    TextField("insert fruit name", text: $fruitName)
+                    Button {
+                        favoriteFruits.append(Fruit(name: fruitName, matchFruitName: "Apple", price: 1000))
+                        fruitName = ""
+                    } label: {
+                        Text("insert")
+                            .padding()
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }.padding()
+
+//                List(favoriteFruits) { fruit in
+//                    HStack {
+//                        Text(fruit.name)
+//                        Spacer()
+//                        Text("\(fruit.price)원")
+//                    }
+//                }
+                List {
+                    ForEach(favoriteFruits) { fruit in
+                        HStack {
+                            Text(fruit.name)
+                            Spacer()
+                            Text("\(fruit.price)원")
+                        }
+                    }
+//                        .onDelete(perform: deleteFruit)
+                    .onDelete{ indexSet in
+                        favoriteFruits.remove(atOffsets: indexSet)
+                    }
                 }
             }
-            .navigationTitle("Fruit List")
+                .navigationTitle("Fruit List")
         }
     }
 }
